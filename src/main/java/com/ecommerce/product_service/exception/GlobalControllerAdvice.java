3d03761b2a69f190a21196380c5b909b08670b0e
Karen Calderon <krcatari@umsa.bo>
2026-06-02 10:03:26 -0400

    validaciones de campos en solicitudes

--- src/main/java/com/ecommerce/product_service/exception/GlobalControllerAdvice.java
+++ src/main/java/com/ecommerce/product_service/exception/GlobalControllerAdvice.java
@@ -1,17 +1,22 @@
 package com.ecommerce.product_service.exception;
 
+import ch.qos.logback.classic.util.LogbackMDCAdapter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import java.net.URI;
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalControllerAdvice {
 
+    // Para manejar errores de recursos no encontrados
     @ExceptionHandler(ResourceNotFoundException.class)
     public ProblemDetail handleResourceNotFoundException(ResourceNotFoundException EX, WebRequest request) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, EX.getMessage());
@@ -26,4 +31,25 @@ public class GlobalControllerAdvice {
 
         return problemDetail;
     }
+
+    // Para manejar errores de validacin de campos en las solicitudes
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException EX) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Validation failed for one or more fields.");
+
+        problemDetail.setTitle("Validation Error");
+        problemDetail.setType(URI.create("http://api.ecommerce.com/errors/validation-error"));
+        problemDetail.setProperty("Timestamp", Instant.now());
+
+        Map<String, String> errorMap = new HashMap<>();
+
+        // Recorremos los errores y los vamos metiendo al mismo mapa
+        EX.getBindingResult().getFieldErrors().forEach(fieldError -> {
+            errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+        });
+        problemDetail.setProperty("errors", errorMap);
+
+        return problemDetail;
+    }
 }
+
